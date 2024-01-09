63a111365d030ddb785d25ee1df403defba1fcca
Matthew Curtis <matthewcurtis@khanacademy.org>
2024-01-09 16:10:52 -0600

    remove broken cross ref

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,18 +1,8 @@
 import { Link } from "react-router-dom";
 
-import v4_1_0 from "../data/shortcuts-v4_1_0";
 import SYNTAX from "../data/syntax";
-import generateCrossReference from "../util/crossRef";
 
 import "./AboutPage.css";
-
-const crossReference = JSON.stringify(
-  generateCrossReference(SYNTAX, v4_1_0),
-  null,
-  2
-);
-const crDataString =
-  "data:text/json;charset=utf-8," + encodeURIComponent(crossReference);
 
 function AboutPage() {
   return (
@@ -61,11 +51,6 @@ function AboutPage() {
         </a>
         .
       </p>
-
-      {/* <h2>Extra</h2>
-      <a href={crDataString} download="cross-reference.json">
-        Download cross-reference as JSON
-      </a> */}
     </div>
   );
 }
